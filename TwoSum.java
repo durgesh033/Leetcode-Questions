5da237c159f3ea1376fc4315dfb53f6a1420d915
durgesh033 <durgeshrout8@gmail.com>
2026-06-24 23:58:51 +0530

    optimized with hashmap

--- TwoSum.java
+++ TwoSum.java
@@ -1,26 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
+        HashMap <Integer, Integer> maps = new HashMap<>();
 
-        int len = nums.length;
-        for(int i = 0; i < len; i++){
-            for(int j=0; j < len; j++) {
-                if (i != j) {
-                    if(nums[i] + nums[j] == target){
-                        arr[0] = i;
-                        arr[1] = j;
-                        return arr;
-                    }
-                }
+        for(int i = 0; i < nums.length; i++) {
+            int required = target - nums[i];
+
+            if (maps.containsKey(required)){
+                return new int[]{maps.get(required), i};
             }
+
+            maps.put(nums[i], i);
         }
-        return arr;
+
+        return new int[] {-1, -1};
     }
 }
-
-
-This is the brute force method i created through my own ideology...
-Time complexity - O(n2) (Not optimized)
-
-It can be optimized more through Hash Map... But right now i don't know how to use hash map
-so tommrow i'll learn that
